050e9df2020758fcd555937a735589e1b149e979
yassin-eltahir <yeltahir@gmail.com>
2016-11-26 16:44:06 +1100

    Add pdf coordinates to script.
    This will later be removed & added to a config file

--- raw_processing/lib/pdf_processing.py
+++ raw_processing/lib/pdf_processing.py
@@ -9,42 +9,48 @@ import pandas as pd
 from PyPDF2 import PdfFileReader
 from tabula import read_pdf_table
 
+# Coordinates for first page table
+y1 = 224.4
+x1 = 11
+y2 = 770
+x2 = 580
+p1_coords = [y1, x1, y2, x2] 
+
+
+# Coordinates for pages 2-n
+y21 = 0
+x21 = 11
+y22 = 780
+x22 = 590
+p2n_coords = [y21, x21, y22, x22]
+
+
 
 # All of the steps required to extract the data table from the PDF
 # Takes the location of a pdf as in input & returns a dataframe with the extracted information
 def pdf_process(pdf):
     
-    try:
-        # Determine City & Auction Date
-        city, date = city_date(pdf)
+    # Determine City & Auction Date
+    city, date = city_date(pdf)
 
-        # Process pages 2-N
-        p2n = process_p2n(pdf, p2n_coords)
-                
-        # Extract Columns to assign to Page 1
-        if p2n is None:
-            p2_columns = None
-        else:
-            p2_columns = p2n.columns
-        
-        # Process page 1
-        p1 = process_p1(pdf, p1_coords, p2_columns)
-        
-        # Combine P1 & P2N and add city & date info
-        out = p1.append(p2n).reset_index()
-        out['city'] = city
-        out['date'] = date
-
-        print "Processed {}".format(pdf)
-        
-        return out
+    # Process pages 2-N
+    p2n = process_p2n(pdf, p2n_coords)
+            
+    # Extract Columns to assign to Page 1
+    if p2n is None:
+        p2_columns = None
+    else:
+        p2_columns = p2n.columns
     
-    except:
-        print "Failed {}".format(pdf)
-        failed.append(pdf)
-        pass
-
-
+    # Process page 1
+    p1 = process_p1(pdf, p1_coords, p2_columns)
+    
+    # Combine P1 & P2N and add city & date info
+    out = p1.append(p2n).reset_index()
+    out['city'] = city
+    out['date'] = date
+    
+    return out
 
 # Determine city & auction date
 def city_date(filename):
