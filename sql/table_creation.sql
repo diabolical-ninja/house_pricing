12dc491b9cb83e871e009db384f16f7efb21d89c
yassin-eltahir <yeltahir@gmail.com>
2019-02-02 17:49:13 +1100

    Add fact tables

--- sql/table_creation.sql
+++ sql/table_creation.sql
@@ -18,3 +18,48 @@ CREATE TABLE real_estate.auction_results (
     date DATE,
     cash_rate FLOAT
 );
+
+
+
+
+/*
+Create definitions for the various keys/symbols present
+*/
+
+-- Results codes
+CREATE TABLE real_estate.fact_result (
+    result VARCHAR,
+    definition VARCHAR
+)
+
+INSERT INTO real_estate.fact_result (result, definition) VALUES
+  ('SS','Sold after auction price not disclosed')
+, ('VB','Vendor bid')
+, ('SN','Sold not disclosed')
+, ('S','Property sold')
+, ('W','Withdrawn prior to auction')
+, ('SA','Sold after auction')
+, ('PN','Sold prior not disclosed')
+, ('SP','Property sold prior')
+, ('PI','Property passed in')
+, ('NB','No bid')
+
+
+-- Property Types
+CREATE TABLE real_estate.fact_property_type (
+    prop_type VARCHAR,
+    definition VARCHAR
+)
+
+INSERT INTO real_estate.fact_property_type (prop_type, definition) VALUES
+  ('t','Townhouse')
+, ('dev site','Development site')
+, ('o res','Other residential')
+, ('u','Unit')
+, ('u','Duplex')
+, ('h','House')
+, ('h','Cottage')
+, ('h','Villa')
+, ('h','Semi')
+, ('h','Terrace')
+, ('studio','Studio')
